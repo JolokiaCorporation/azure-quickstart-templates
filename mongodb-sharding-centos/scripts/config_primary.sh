e1f0c2b1543e6f3ec5e1f4d78a93b6ce3bce2323
Mark Pace <pace@illuminati.org>
2021-06-11 12:36:18 -0700

    Update config_primary.sh
    changing to mongo 4.4

--- mongodb-sharding-centos/scripts/config_primary.sh
+++ mongodb-sharding-centos/scripts/config_primary.sh
@@ -6,10 +6,10 @@ zabbixServer=$3
 
 install_mongo3() {
 #create repo
-cat > /etc/yum.repos.d/mongodb-org-3.2.repo <<EOF
-[mongodb-org-3.2]
+cat > /etc/yum.repos.d/mongodb-org-4.4.repo <<EOF
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.4/x86_64/
 gpgcheck=0
 enabled=1
 EOF
@@ -71,7 +71,7 @@ install_zabbix() {
 }
 
 install_mongo3
-install_zabbix
+#install_zabbix
 
 
 
