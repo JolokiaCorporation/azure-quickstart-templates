74021509180132945db7d80fa78e20c035b4ee19
Mark Pace <pace@illuminati.org>
2021-06-11 12:39:25 -0700

    Update router1.sh

--- mongodb-sharding-centos/scripts/router1.sh
+++ mongodb-sharding-centos/scripts/router1.sh
@@ -4,12 +4,12 @@ mongoAdminUser=$1
 mongoAdminPasswd=$2
 zabbixServer=$3
 
-install_mongo3() {
+install_mongo4() {
 #create repo
-cat > /etc/yum.repos.d/mongodb-org-3.2.repo <<EOF
-[mongodb-org-3.2]
+cat > /etc/yum.repos.d/mongodb-org-4.4.repo <<EOF
+[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/3.2/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.4/x86_64/
 gpgcheck=0
 enabled=1
 EOF
@@ -78,8 +78,8 @@ install_zabbix() {
 }
 
 
-install_mongo3
-install_zabbix
+install_mongo4
+#install_zabbix
 
 
 #start router server
